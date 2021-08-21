bcece65b85673bea628205c60633a37db47360cf
Andrei Neacsu <andnea823@gmail.com>
2021-08-21 13:00:19 -0500

    Upload Files

--- src/main/java/b1pod/Bot.java
+++ src/main/java/b1pod/Bot.java
@@ -1,6 +1,7 @@
 package b1pod;
 
 import b1pod.Commands.*;
+import b1pod.Commands.UserTags;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
