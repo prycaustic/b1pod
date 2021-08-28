159f36479591247b106392c32d426486bca3712b
Andrei Neacsu <andnea823@gmail.com>
2021-08-28 11:30:59 -0500

    redo user tags command LOL

--- src/main/java/b1pod/Commands/UserTags2.java
+++ src/main/java/b1pod/Commands/UserTags2.java
@@ -1,74 +1,161 @@
 package b1pod.Commands;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+
+import static b1pod.Bot.getEmote;
 
 public class UserTags2 extends ListenerAdapter
 {
+    private final String MYSQL_PWD;
+
+    public UserTags2(String password)
+    {
+        MYSQL_PWD = password;
+    }
+
     @Override
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event)
     {
+        if (event.getAuthor().isBot()) return;
         String content = event.getMessage().getContentRaw();
 
-        if (!event.getAuthor().isBot())
+        if (content.startsWith("/tag"))
         {
-            if (content.startsWith("/tag"))
+            String[] args = content.split(" (?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+            for (int i = 0; i < args.length; i++)
             {
-                String[] args = content.split(" (?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+                args[i] = args[i].replace("\"", "");
+            }
 
-                switch (args[1])
+            if (tagsEnabled(event.getGuild()))
+            {
+                try
                 {
-                    case "add":
-                        addTag(args[2], args[3]);
-                        break;
-                    case "remove":
-                        removeTag(args[2]);
-                        break;
+                    switch (args[1])
+                    {
+                        case "add":
+                            addTag(event.getMessage(), args[2], args[3]);
+                            break;
+                        case "remove":
+                            removeTag(args[2]);
+                            break;
+                    }
                 }
+                catch (IndexOutOfBoundsException e)
+                {
+                    event.getMessage().reply("Incorrect syntax, use `/tag help` for more info.")
+                            .mentionRepliedUser(false).queue();
+                }
+            }
+            else if (args[1].equals("enable"))
+            {
+                enableTags(event.getMessage());
             }
             else
             {
-                searchForTag(content);
+                event.getMessage()
+                        .reply("Tags are not enabled in this server. Use `/tag enable` to enable.")
+                        .mentionRepliedUser(false).queue();
+            }
+        }
+        else
+        {
+            if (tagsEnabled(event.getGuild()))
+            {
+                String tag = searchForTag(event.getChannel(), content);
+
+                if (tag != null)
+                    event.getChannel().sendMessage(tag).queue();
             }
         }
     }
 
-    private static Connection connect(String host, String username, String password) throws SQLException
+    private void enableTags(Message message)
     {
-        return DriverManager.getConnection(host, username, password);
+        try
+        {
+            Connection conn = connect();
+
+            if (conn != null)
+            {
+                String query = "CREATE TABLE `test_schema`.`g" + message.getGuild().getId() + "` (\n" +
+                        "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                        "  `name` VARCHAR(99) NOT NULL,\n" +
+                        "  `value` VARCHAR(200) NOT NULL,\n" +
+                        "  PRIMARY KEY (`id`));\n";
+
+                conn.prepareStatement(query).executeUpdate();
+                message.addReaction(getEmote("success")).queue();
+            }
+        }
+        catch (SQLException e)
+        {
+            message.addReaction(getEmote("failure")).queue();
+            e.printStackTrace();
+        }
     }
 
-    private static ResultSet getSearchResult(Connection conn, String message) throws SQLException
+    private Connection connect() throws SQLException
     {
-        String query = "SELECT * FROM tags" +
-                "WHERE name=\"" + message + "\"";
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test_schema", "root", MYSQL_PWD);
+    }
+
+    private ResultSet getSearchResult(Connection conn, String table, String message) throws SQLException
+    {
+        String query = "SELECT * FROM " + table + " WHERE name=\"" + message + "\"";
 
         return conn.prepareStatement(query).executeQuery();
     }
 
-    private static void addTag(String name, String tag)
+    private void addTag(Message message, String name, String tag)
     {
+        try
+        {
+            Connection conn = connect();
 
+            if (conn != null)
+            {
+                if (searchForTag(message.getTextChannel(), name) == null)
+                {
+                    String query = "INSERT INTO g" + message.getGuild().getId() + " (id, name, value)" +
+                            "\nVALUES (NULL, \"" + name + "\", \"" + tag + "\");";
+
+                    conn.prepareStatement(query).executeUpdate();
+                    message.addReaction(getEmote("success")).queue();
+                }
+                else
+                {
+                    message.reply("Tag already exists. Please delete the original first.")
+                            .mentionRepliedUser(false).queue();
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            message.addReaction(getEmote("failure")).queue();
+            e.printStackTrace();
+        }
     }
 
-    private static void removeTag(String name)
+    private void removeTag(String name)
     {
         System.out.println("removing...");
     }
 
-    private static void searchForTag(String content)
+    private String searchForTag(TextChannel channel, String content)
     {
         Connection conn = null;
 
         try
         {
-            conn = connect("jdbc:mysql://localhost", "root", "");
+            conn = connect();
         }
         catch (SQLException e)
         {
@@ -79,9 +166,33 @@ public class UserTags2 extends ListenerAdapter
         {
             try
             {
-                ResultSet result = getSearchResult(conn, content);
+                ResultSet result = getSearchResult(conn, "g" + channel.getGuild().getId(), content);
+
+                if (result.next())
+                    return result.getString("value");
             }
             catch (SQLException ignored) { }
         }
+        return null;
+    }
+
+    private boolean tagsEnabled(Guild guild)
+    {
+        try
+        {
+            Connection conn = connect();
+
+            if (conn != null)
+            {
+                DatabaseMetaData meta = conn.getMetaData();
+                ResultSet result = meta.getTables(null, null, "g" + guild.getId(), null);
+
+                if (result.next())
+                    return true;
+            }
+        }
+        catch (SQLException ignored) { }
+
+        return false;
     }
 }
