a3a8ace4c9b11b30a6483be16ba0e5ae7dec7bb9
Andrei Neacsu <andnea823@gmail.com>
2021-10-15 10:41:38 -0500

    Fix tag list command, also bring Kanye back to life

--- src/main/java/b1pod/commands/UserTags3/TagList.java
+++ src/main/java/b1pod/commands/UserTags3/TagList.java
@@ -26,15 +26,33 @@ public class TagList extends Command
     {
         String guildId = event.getGuild().getId();
         if (!tagsEnabled(guildId)) return new ExecutionResult("failure", "Tags are not enabled in this server.");
+
         String query = "SELECT * FROM g" + guildId;
         ResultSet result = retrieve(query);
-        EmbedBuilder listEmbed = new EmbedBuilder().setTitle("Tags").setColor(getEmbedColor())
-                .setDescription("No tags found");
+        result.last();
+        int pages = (result.getRow() / 9) + 1;
+        int page = 1, startIndex = 1;
+        if (args.length == 3)
+        {
+            page = Integer.parseInt(args[2]);
+            startIndex = page * 9 - 9;
+        }
+        if (startIndex < 0 || page > pages)
+            return new ExecutionResult("failure", "Page does not exist.");
 
-         while (result.next())
+        EmbedBuilder listEmbed = new EmbedBuilder().setTitle("Tags").setColor(getEmbedColor())
+                .setDescription("No tags found")
+                .setFooter("Page: " + page + " of " + pages);
+        int counter = startIndex;
+
+        // Go to the correct starting position
+        result.absolute(startIndex);
+
+         while (result.next() && counter < startIndex + 9)
          {
              listEmbed.setDescription("");
              listEmbed.addField(result.getString("name"), result.getString("value"), true);
+             counter++;
          }
 
         return new ExecutionResult(listEmbed.build());
